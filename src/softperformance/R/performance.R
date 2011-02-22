5d6b9a3863ac2ab355fa37f8c97f3fae6eb874d1
cbeleites <cbeleites@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-22 20:23:43 +0000

    tests for sens
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/softclassval@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/softperformance/R/performance.R
+++ src/softperformance/R/performance.R
@@ -9,7 +9,6 @@
 ##' \code{r} must have the same number of rows as \code{p}, all other dimensions may be filled by recycling. If the c
 ##'
 ##' @rdname performance
-##' @param ... ensures that arguments are named
 ##' @param r vector, matrix, or array with reference. 
 ##' @param p vector, matrix, or array with predictions
 ##' @param group grouping variable for the averaging by \code{\link[base]{rowsum}}. If \code{NULL},
@@ -24,15 +23,11 @@
 ##' @references see the literature in \code{citation ("softperformance")}
 ##' @export
 ##' @include softperformance.R
-sens <- function (..., r, p, group = NULL,
+sens <- function (r = stop ("missing reference"), p = stop ("missing prediction"),
+                  group = NULL,
                   operator = "prd", op.dev = dev (operator), op.postproc = postproc (operator),
                   eps = 1e-8){
 
-  ## make sure the arguments are correctly named
-  dots <- list (...)
-  if (length (dots) > 0L)
-    stop ("Unknown arguments:", names (dots))
-  
   operator <- match.fun (operator)
   force (op.dev)
   force (op.postproc)
@@ -44,8 +39,10 @@ sens <- function (..., r, p, group = NULL,
     POSTFUN <- match.fun (op.postproc)
 
   ## check prediction and reference
-  dr <- dim (r);  if (is.null (dr)) dr <- length (r)
-  dp <- dim (p);  if (is.null (dp)) dp <- length (p)
+  r <- ensuredim (r)
+  p <- ensuredim (p)
+  dr <- dim (r)
+  dp <- dim (p)
 
   rec <- prod (dp [- seq_along (dr)])
   dp <- dp [seq_along (dr)]
@@ -90,5 +87,87 @@ sens <- function (..., r, p, group = NULL,
     res <- 1 - res
 
   res
+}
+
+.test (sens) <- function (){
+  ops <- c ("luk", "gdl", "prd", "and", "wMAE", "wMSE", "wRMSE")
+
+  ## shape & names
+  for (o in ops){
+    ## vector
+    tmp <- sens (r = v, p = v, operator = o)
+    checkEquals (dim (), 1L)
+    checkTrue (is.null (dimnames (tmp))[[1L]])
+    checkTrue (is.null (names (tmp)))
+
+    ## matrix
+    tmp <- sens (r = v [1 : 4], p = m, operator = o)
+    checkEquals (dim (tmp), c(1L, ncol (m)))
+    checkEquals (dimnames (tmp), list (NULL, colnames (m)))
+    checkTrue (is.null (names (tmp)))
+    
+    
+    ## array
+    tmp <- sens (r = rep (v [1 : 5], 2), p = pred.array, operator = o)
+    checkEquals (dim (tmp), c(1L, ncol (m)))
+    checkEquals (dimnames (tmp), list (NULL, colnames (m)))
+    checkTrue (is.null (names (tmp)))
+    
+    
+  }
+  
+  checkEquals (sens(r = ref, p = ref),
+               structure (c (0.85, 0.4, NA), .Dim = c(1L, 3L),
+                          .Dimnames = list(NULL, c("A", "B", "C"))))
+  checkEquals (sens (r = ref, p = ref, group = rep (c ("H", "S"), each = 5)),
+               structure(c(1, 0.6, NA, 0.4, NA, NA), .Dim = 2:3,
+                         .Dimnames = list(c("H", "S"), c("A", "B", "C"))))
+
+  checkEquals (sens (r = ref, p = ref, operator="gdl"),
+               structure(c(1, 1, NA), .Dim = c(1L, 3L),
+                         .Dimnames = list(NULL, c("A", "B", "C"))))
+
+  checkEquals (sens (r = ref, p = ref, group = rep (c ("H", "S"), each = 5), operator="gdl"),
+               structure (c (1, 1, NA, 1, NA, NA), .Dim = 2:3,
+                          .Dimnames = list(c ("H", "S"), c("A", "B", "C"))))
+
+
+  checkEquals (sens (r = ref, p = ref, operator="luk"),
+               structure (c (0.75, 0, NA), .Dim = c(1L, 3L),
+                          .Dimnames = list(NULL, c("A", "B", "C"))))
+  checkEquals (sens (r = ref, p = ref, group = rep (c ("H", "S"), each = 5), operator="luk"),
+               structure (c (1, 0.333333333333333, NA, 0, NA, NA), .Dim = 2:3,
+                          .Dimnames = list (c ("H", "S"), c("A", "B", "C"))))
+
+  checkEquals (sens (r = ref, p = ref, operator="wMAE"),
+               structure(c(1, 1, NA), .Dim = c(1L, 3L),
+                         .Dimnames = list(NULL, c("A", "B", "C"))))
+  checkEquals (sens (r = ref, p = ref, group = rep (c ("H", "S"), each = 5), operator="wMAE"),
+               structure (c (1, 1, NA, 1, NA, NA), .Dim = 2:3,
+                          .Dimnames = list(c ("H", "S"), c("A", "B", "C"))))
+
+  checkEquals (sens (r = ref, p = ref, operator="wMSE"),
+               structure(c(1, 1, NA), .Dim = c(1L, 3L),
+                         .Dimnames = list(NULL, c("A", "B", "C"))))
+  checkEquals (sens (r = ref, p = ref, group = rep (c ("H", "S"), each = 5), operator="wMSE"),
+               structure (c (1, 1, NA, 1, NA, NA), .Dim = 2:3,
+                          .Dimnames = list(c ("H", "S"), c("A", "B", "C"))))
+
+  checkEquals (sens (r = ref, p = ref, operator="wRMSE"),
+               structure(c(1, 1, NA), .Dim = c(1L, 3L),
+                         .Dimnames = list(NULL, c("A", "B", "C"))))
+  
+  checkEqualsNumeric (sens (r = ref, p = ref, group = rep (c ("H", "S"), each = 5), operator="wRMSE"),
+                      c (1,  1,
+                         NA, 1,
+                         NA, NA))
+
+
+  tmp <- pred
+  tmp [which (ref == 0)] <- NA          # which keeps the attributes
+  
+  checkEqualsNumeric (sens (r = ref, p = p, operator="prd", group = 1:10), tmp) 
+  sens (r = ref, p = p, operator="prd", group = rep (1L : 2, each = 5))
+  sens (r = ref, p = p, operator="prd", group = rep (1L : 5, 2))             
 }
 
