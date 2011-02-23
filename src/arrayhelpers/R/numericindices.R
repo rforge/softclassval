733bbd4eb91e8f2f527b521610e8b49caf41c2aa
cbeleites <cbeleites@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-23 23:05:47 +0000

    bugfix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/softclassval@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/arrayhelpers/R/numericindices.R
+++ src/arrayhelpers/R/numericindices.R
@@ -5,18 +5,21 @@
 ##' @return numeric indices
 ##' @author Claudia Beleites
 ##' @export 
-numericindex <- function (x, n = names (x), indices){
+numericindex <- function (x, indices, n = names (x)){
   if (is.character (indices))
     match (indices, n)
   else if (is.logical (indices))
-    which (indices)
+    seq_along (x) [indices]
   else if (is.numeric (indices))
     indices
-  else
+   else
     stop ("indices must be numeric, logical, or character")
 }
 .test (numericindex) <- function (){
-  checkEquals (numericindex (a, names (dimnames (a)), c("rows", "d3", "d4")), c (1L, 3L, NA))
-  checkEquals (numericindex (a, names (dimnames (a)), c(TRUE, FALSE, TRUE)), c(1L, 3L))
-  checkEquals (numericindex (a, names (dimnames (a)), 1L), 1L)
+  checkEquals (numericindex (v, c("b", "a", "x")), c (2L, 1L, NA))
+  checkEquals (numericindex (v, c(TRUE, FALSE, TRUE)), c(1L, 3L))
+  checkEquals (numericindex (v, TRUE), 1 : 3)
+  checkEquals (numericindex (v, FALSE), integer (0L))
+  checkEquals (numericindex (v, c(TRUE, FALSE)), c(1L, 3L))
+  checkEquals (numericindex (v, 1L), 1L)
 }
