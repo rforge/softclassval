1842180e8ceed0619c255036b805650da0f836fb
cbeleites <cbeleites@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-14 14:14:06 +0000

    moved .make01 to hardclasses.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/softclassval@94 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/R/operators.R
+++ src/R/operators.R
@@ -104,22 +104,6 @@ test (prd) <- function(){
   checkEqualsNumeric (prd (v, rev (v)), c (a = NA, b = 0.3, c = 0.49, d = 0.3, e = NA))
 }
 
-## helper for "and" operator: sets all values NA that are not within +- tol from 0 or 1.
-.make01 <- function (x, tol = 1e-6){
-  tmp <- rep (NA_real_, length (x))
-  tmp [x >    -tol & x <     tol] <- 0
-  tmp [x > 1 - tol & x < 1 + tol] <- 1
-  
-  attributes (tmp) <- attributes (x)
-
-  tmp
-}
-test (.make01) <- function (){
-  checkIdentical (.make01 (v), c( a = 0, b = NA, c = NA, d = 1, e = NA))
-  checkIdentical (attributes (.make01 (m)), attributes (m))
-}
-
-
 ##' @rdname operators
 ##' @export 
 and <- function (r, p){ # the boolean and: accepts only hard r and p
