6839409a238f85c0bc222c6e6f7f3d6b48052ebe
cbeleites <cbeleites@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-29 14:13:40 +0000

    build
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/softclassval@108 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/init.R
+++ pkg/R/init.R
@@ -4,7 +4,7 @@
   desc <- utils::packageDescription("softclassval")
   vers <- paste("V. ", desc$Version)
 
-  cat ("Package ",  desc$Package, ", version ", desc$Version, "\n\n",
+  packageStartupMessage ("Package ",  desc$Package, ", version ", desc$Version, "\n\n",
        "If you use this package please cite it appropriately.\n",
        "   citation(\"", desc$Package, "\")\nwill give you the correct reference.", "\n\n",
        "The project homepage is ", desc$URL, "\n\n",
